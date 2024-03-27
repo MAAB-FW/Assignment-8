bcd9db61f850d73b99c734bf673be330987d5439
maab-fw <maabfw@proton.me>
2024-03-27 11:35:06 +0600

    responsive banner text fixed

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
     return (
         <div className="bg-[#1313130D] lg:h-[554px] rounded-3xl flex flex-col-reverse lg:flex-row items-center justify-center lg:gap-[86px]">
             <div className="flex mb-9 lg:mb-0 mt-8 lg:mt-0 flex-col lg:gap-12 items-center lg:items-start text-center lg:text-left justify-center">
-                <h2 className="font-bold text-[25px] lg:text-[56px] lg:w-[526px] text-[#131313]">
+                <h2 className="font-bold text-[25px] lg:text-[56px] w-[90%] lg:w-[526px] text-[#131313]">
                     Books to freshen up your bookshelf
                 </h2>
                 <NavLink to={"/listed-books"} className="btn bg-[#23BE0A] text-white">
