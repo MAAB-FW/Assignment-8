088f512b5531a4f6d0a911bd60817372c5d8ef65
maab-fw <maabfw@proton.me>
2024-03-27 10:05:31 +0600

    static about page context added.

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,17 @@
 const About = () => {
     return (
-        <div className=" flex justify-center items-center text-4xl h-[73vh] animate-pulse">
-            <h2>This is Static About Page</h2>
+        <div className=" flex flex-col justify-start items-center h-[73vh] ">
+            <h2 className="text-4xl animate-pulse font-black pb-2 border-b-4 border-black">About For Book Vibe</h2>
+            <p className="mt-9 text-lg text-center w-[80%]">
+                To people who value knowledge, Book Vibe is the platform that makes learning anything easy because it transforms
+                the hard-to-understand into easy-to-use. Through expert editorial, engaging experiences and an approachable style,
+                learners at every level can confidently use their knowledge to fuel their pursuit of professional advancement and
+                personal betterment. With a commitment to maintaining the highest editorial standards and a promise to continue
+                developing new, innovative digital experiences, Book Vibe makes learning anything even easier.
+            </p>
+            <p className="mt-8">
+                <em>Success is for Book Vibe.</em>
+            </p>
         </div>
     )
 }
